5036bdb8e11be4a0c5537ac94de8e95aae02c4ac
dongrui <dongrui23@163.com>
2022-03-20 22:24:55 +0800

    图片

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -79,16 +79,10 @@ module.exports = {
     },
     "friendLink": [
       {
-        "title": "午后南杂",
+        "title": "dog",
         "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
+        "email": "dongrui23@163.com",
+        "link": "https://github.com/dong2r"
       }
     ],
     "logo": "/me.jpeg",
--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 home: true
 heroText: 正是修行时。
 tagline: It is practicing.
-# heroImage: /hero.png
-# heroImageStyle: {
-#   maxWidth: '600px',
-#   width: '100%',
-#   display: block,
-#   margin: '9rem auto 2rem',
-#   background: '#fff',
-#   borderRadius: '1rem',
-# }
+heroImage: /background.jpeg
+heroImageStyle: {
+  maxWidth: '600px',
+  width: '100%',
+  display: block,
+  margin: '9rem auto 2rem',
+  background: '#fff',
+  borderRadius: '1rem',
+}
 bgImageStyle: {
   height: '450px'
 }
