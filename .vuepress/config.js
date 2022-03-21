05ef57176e8ed88d197c4298b6de24cd4b9ca0a6
dongrui <dongrui23@163.com>
2022-03-21 10:23:58 +0800

    链接

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -42,23 +42,74 @@ module.exports = {
         "icon": "reco-date"
       },
       {
-        "text": "文档",
-        "icon": "reco-message",
+        "text": "工具箱",
+        "icon": "reco-category",
         "items": [
           {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
+            "text": "Pdf工具",
+            "link": "https://pdf.io/"
+          },
+          {
+            "text": "动漫",
+            "link": "https://www.agefans.cc/"
+          },
+          {
+            "text": "插件",
+            "link": "https://chrome.zzzmh.cn/#ext"
+          },
+          {
+            "text": "Json解析",
+            "link": "http://tool.mkblog.cn/json/"
+          },
+          {
+            "text": "壁纸",
+            "link": "https://wallhaven.cc/"
+          },
+          {
+            "text": "AI写作",
+            "link": "https://xiezuocat.com/#/?s=ilxdh"
+          },
+          {
+            "text": "PPT",
+            "link": "https://pptsupermarket.com/"
+          },
+          {
+            "text": "Maven仓库",
+            "link": "https://developer.aliyun.com/mvn/search"
+          },
+          {
+            "text": "转在线img",
+            "link": "https://imgtu.com/"
+          },
+          {
+            "text": "科学上网",
+            "link": "https://gitee.com/zeng-chaofan/awesome-vpn?_from=gitee_search"
           }
         ]
       },
       {
         "text": "関纡莪",
-        "icon": "reco-message",
+        "icon": "reco-account",
         "items": [
           {
             "text": "GitHub",
             "link": "https://github.com/dong2r/blog",
             "icon": "reco-github"
+          },
+          {
+            "text": "Gitee",
+            "link": "https://gitee.com/idogrui",
+            "icon": "reco-mayun"
+          },
+          {
+            "text": "bilibili",
+            "link": "https://space.bilibili.com/491471655?spm_id_from=333.337.0.0",
+            "icon": "reco-bilibili"
+          },
+          {
+            "text": "博客园",
+            "link": "https://www.cnblogs.com/drsyq/",
+            "icon": "reco-bokeyuan"
           }
         ]
       }
